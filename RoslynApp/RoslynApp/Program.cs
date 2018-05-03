3746834e8f237a6b3cdf838f0d62749e9ca225a9
alexdeyneko <alfire17496@gmail.com>
2018-05-03 21:37:13 +0500

    * add simple option of RoslynCalculator

---
+++ RoslynApp/RoslynApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace RoslynApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain domain = AppDomain.CreateDomain("test");
+            domain.Load(Assembly.GetExecutingAssembly().GetName());
+            var calculator = (Calculator)domain.CreateInstanceAndUnwrap(
+                Assembly.GetExecutingAssembly().FullName, "RoslynApp.Calculator");
+            for (;;)
+            {
+                string expession = Console.ReadLine();
+                calculator.Calculate(expession);
+            }
+        }
+    }
+
+    class Calculator : System.MarshalByRefObject
+    {
+        async public void Calculate(string expression)
+        {
+            var result =  await CSharpScript.EvaluateAsync(expression, ScriptOptions.Default.WithImports("System.Math"));
+            WriteResult(result.ToString());
+        }
+        private void WriteResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+    
+}
